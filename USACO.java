0e3c89150f9cc3c49bb0e12fd0df54a0ae42ef4f
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-03-10 12:35:38 -0400

    Update USACO.java

--- USACO.java
+++ USACO.java
@@ -1,47 +1,80 @@
+import java.util.*;
+import java.io.*;
+
 public class USACO{
-  public static int bronze(String filename){
-    int r, c, e, n;
-    File lake = new File(filename);
-    Scanner lakeler = new Scanner(lake);
-    r = lakeler.nextInt();
-    c = lakeler.nextInt();
-    e = lakeler.nextInt();
-    n = lakeler.nextInt();
-    lakeler = new Scanner(lake);
-    lakeler.nextLine();
-    String n = lakeler.nextLine();
-    lakeler = new Scanner(lake);
-    int a = 0;
-    while (lakeler.hasNextLine()){
-      lakeler.nextLine();
-      a++;
-    }
-    lakeler = new Scanner(lake);
-    int[][] elevations = new int[n.length()][a - 1 - n];
-    int[][] directions;
-    private int[][] stomp(int r, int c, int inches){
-     private int[]gridElevations = new int[9];
-       gridElevations[0] = elevations[r][c];
-        gridElevations[1] = elevations[r+1][c];
-        gridElevations[2] = elevations[r+2][c];
-        gridElevations[3] = elevations[r+1][c+1];
-        gridElevations[4]= elevations[r+1][c+2];
-        gridElevations[5] = elevations[r+2][c+1];
-        gridElevations[6] = elevations[r+2][c+2];
-        gridElevations[7] = elevations[r][c+1];
-        gridElevations[8] = elevations[r][c+2];
-      int max = gridElevations[0];
-      for(int i = 0; i < gridElevations.length;i++){
-        if(gridElevations[i] > max)
-          max = gridElevations[i];
+  public static int bronze(String filename) throws FileNotFoundException{
+    File file = new File(filename);
+    Scanner scan = new Scanner(file);
+
+    String[] RCEN = scan.nextLine().split(" ");
+    int r = Integer.parseInt(RCEN[0]);
+    int c = Integer.parseInt(RCEN[1]);
+    int e = Integer.parseInt(RCEN[2]);
+    int n = Integer.parseInt(RCEN[3]);
+
+    int[][] lake = new int[r][c];
+    for(int i = 0; i < r; i++){
+      for (int j = 0; j < c; j++){
+        lake[i][j] = scan.nextInt();
       }
-      int height = max - inches;
-      
     }
- }
 
-  public static int silver(String filename){
-    
+    int[][] directions = new int[n][3];
+    for(int i = 0; i < n; i++){
+      for (int i2 = 0; i2 < 3; i2++){
+        directions[i][i2] = scan.nextInt();
+        System.out.println(directions[i][i2]);
+      }
+    }
+
+    for (int dir = 0; dir < directions.length; dir++){
+      int row = directions[dir][0] - 1;
+      int col = directions[dir][1] - 1;
+      int subtract = directions[dir][2];
+      int max = 0;
+
+      for (int i = row; i-row < 3 && i < r; i++){
+        for (int j = col; j-col < 3 && j < c; j++){
+          if (lake[i][j] > max){
+            max = lake[i][j];
+          }
+        }
+      }
+
+      max -= subtract;
+
+      for (int i = row; i-row < 3 && i < r; i++){
+        for (int j = col; j-col < 3 && j < c; j++){
+          if (lake[i][j] > max){
+            lake[i][j] = max;
+          }
+        }
+      }
+
+      for (int r1 = 0; r1 < lake.length; r1++){
+        for (int c1 = 0; c1 < lake[0].length; c1++){
+          System.out.print(lake[r1][c1] + " ");
+        }
+        System.out.print("\n");
+      }
+    }
+
+
+    int totDepth = 0;
+    for (int row = 0; row < lake.length; row++){
+      for (int col = 0; col < lake[0].length; col++){
+        if (e - lake[row][col] > 0){
+          totDepth += e - lake[row][col];
+        }
+      }
+    }
+
+    return totDepth * 72 * 72;
   }
-
+  
+   public static int silver(String filename) throws FileNotFoundException{
+    File file = new File(filename);
+    Scanner scan = new Scanner(file);
+     
+   }
 }
